c56f0eacf5f96df21dea82b249e0da7240aa817a
unknown <nforesperance1@gmail.com>
2020-01-20 12:10:17 +0100

    Second Commit

--- async.py
+++ async.py
@@ -3,18 +3,6 @@
 import numpy as np
 import multiprocessing as mp
 
-pool = mp.Pool(mp.cpu_count())
-
-
-# Prepare data
-np.random.RandomState(100)
-arr = np.random.randint(0, 10, size=[200000, 5])
-data = arr.tolist()
-data[:5]
-print(data[:5])
-
-
-results = []
 
 # Step 1: Redefine, to accept `i`, the iteration number
 def load(i, row, minimum, maximum):
@@ -32,16 +20,34 @@ def call_back(result):
 
 
 # Step 3: Use loop to parallelize
-def run ():
-    for i, row in enumerate(data):
+def run (pool,arr):
+    for i, row in enumerate(arr):
         pool.apply_async(load, args=(i, row, 4, 8), callback=call_back)
-
-# Step 4: Close Pool and let all the processes complete    
-pool.close()
-pool.join()  # postpones the execution of next line of code until all processes in the queue are done.
 
 # # Step 5: Sort results [OPTIONAL]
 # results.sort(key=lambda x: x[0])
 # results_final = [r for i, r in results]
-if __name__ == '__main__':  
-    run()
+import multiprocessing
+import time
+
+def calc_square(numbers, q):
+    for n in numbers:
+        q.put(n*n)
+        time.sleep(0.2)
+
+    q.put(-1)
+    print('Exiting function')
+
+if __name__ == '__main__':
+    # Prepare data
+    np.random.RandomState(100)
+    arr = np.random.randint(0, 10, size=[200000, 5])
+    data = arr.tolist()
+    sample = data[:5]
+    print(data[:5])
+
+
+    pool = mp.Pool(mp.cpu_count())
+    run(pool,sample)
+    pool.close()
+    pool.join()
--- test.py
+++
@@ -1,5 +0,0 @@
-import modules.async as asyn
-import sync
-
-if __name__ == '__main__':    
-    extractor = asyn.run()
